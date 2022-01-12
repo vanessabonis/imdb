7d70faa7f9c889a71886c54eb725523f620426a7
vanessabonis <vanessa1302@gmail.com>
2022-01-12 06:35:14 -0300

    MovieCard

--- frontend/src/pages/Listing/listing.tsx
+++ frontend/src/pages/Listing/listing.tsx
@@ -1,8 +1,29 @@
 import Pagination from "components/Pagination/pagination";
+import MovieCard from "components/MovieCard/card";
 
 function Listing() {
     return (
-        <Pagination />
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+        </>
+
     );
 }
 
